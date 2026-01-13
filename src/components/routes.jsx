a8f3500462c35451c062e69f4998028f314d9ec0
Central3 <pawarrsumukha@gmail.com>
2026-01-13 23:13:49 +0530

    move route definitions to separate file and add error page

---
+++ src/components/routes.jsx
@@ -0,0 +1,28 @@
+import shoppingItems from "../utils/fetchItems.js";
+import App from "../App.jsx";
+import Home from "./Home.jsx";
+import Shop from "./Shop.jsx";
+import Cart from "./Cart.jsx";
+import ErrorPage from "./ErrorPage.jsx";
+
+const routes = [
+  {
+    path: "/",
+    loader: async () => {
+      return { data: await shoppingItems() };
+    },
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "home", element: <Home /> },
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+      { path: "cart", element: <Cart /> },
+    ],
+  },
+];
+
+export default routes;
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import App from "./App.jsx";
-import Home from "./components/Home.jsx";
-import Shop from "./components/Shop.jsx";
-import Cart from "./components/Cart.jsx";
+import routes from "./components/routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "home", element: <Home /> },
-      { path: "shop", element: <Shop /> },
-      { path: "cart", element: <Cart /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
